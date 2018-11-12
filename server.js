6f6922615c3472ee4355d17c0a8bfa2efdf34f9a
Eric Wagner <git@alphamule.com>
2018-11-12 12:30:32 -0800

    fix server.js for glitch.com

--- server.js
+++ server.js
@@ -1,30 +1,16 @@
-// server.js
+const express = require('express');
+const app = express();
 
-// init project
-var express = require('express');
-var app = express();
+app.use(express.static('public'));
 
-// http://expressjs.com/en/starter/static-files.html
-app.use(express.static('dist'));
+app.use((req, res, next) => {
+  res.set('Cache-Control', 'no-cache');
+  next();
+});
 
-// // http://expressjs.com/en/starter/basic-routing.html
-// app.get('/', function (request, response) {
-//   response.sendFile(__dirname + '/views/index.html');
-// });
-// 
-// app.get('/about.html', function(request, response) {
-//   response.sendFile(__dirname + '/views/about.html');
-// });
-
-// get our resolutions json
-// var resolutions = require('./resolutions.json');
-
-// here's an endpoint that will return our resolution info
-// app.get('/resolution-data', function(request, response) {
-  // response.send(resolutions);
-// });
+app.use(express.static('dist'))
 
 // listen for requests :)
-var listener = app.listen(3000, function () {
+const listener = app.listen(3000, () => {
   console.log('Your app is listening on port ' + listener.address().port);
 });
